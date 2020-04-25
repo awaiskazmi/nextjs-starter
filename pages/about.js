05aa21fb94b1a0680694b56ae2dc4dd4f60bf52a
Awais Kazmi <devs.awais@gmail.com>
2020-04-26 03:32:30 +0500

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,50 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function About() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">
+          About Us
+        </h1>
+      </main>
+
+      <nav>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </nav>
+
+      <footer>
+        <a
+          href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
+        </a>
+      </footer>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
