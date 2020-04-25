3374190e110b5252490afcf4fd084995da08cc47
Awais Kazmi <devs.awais@gmail.com>
2020-04-26 04:02:30 +0500

    checkout develop branch

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Nextjs Starter</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
